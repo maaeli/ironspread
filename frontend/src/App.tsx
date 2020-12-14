f1c90b6e774064b1580d9c18fc32b94a6f99e339
Martha Brennich <martha.brennich@googlemail.com>
2020-12-14 20:05:46 +0100

    Only App function too long remains as lint error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -85,14 +85,20 @@ const Item: FunctionComponent<ItemProps> = ({
   item,
   onRemoveItem,
 }: ItemProps) => {
-  const { title, url, author, num_comments, points, objectID } = item;
+  const { title, url, author, num_comments, points } = item;
   return (
     <div>
-      <span><a href={url}>{title}</a></span>
+      <span>
+        <a href={url}>{title}</a>
+      </span>
       <span>{author}</span>
       <span>{num_comments}</span>
       <span>{points}</span>
-      <span><button type="button" onClick={() => onRemoveItem(item)}>Dismiss</button></span>
+      <span>
+        <button type="button" onClick={() => onRemoveItem(item)}>
+          Dismiss
+        </button>
+      </span>
     </div>
   );
 };
@@ -100,118 +106,129 @@ const Item: FunctionComponent<ItemProps> = ({
 type ListProps = {
   list: Array<article>;
   onRemoveItem: (item: article) => void;
-}
+};
 
-const List: FunctionComponent<ListProps>  = ({list, onRemoveItem}: ListProps) => (<>
-    {list.map((item: article) => (<Item key={item.objectID} item={item} onRemoveItem={onRemoveItem}/>))}  </>);
-
+const List: FunctionComponent<ListProps> = ({
+  list,
+  onRemoveItem,
+}: ListProps) => (
+  <>
+    {list.map((item: article) => (
+      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+    ))}
+  </>
+);
 
 type Table1props = {
-  header: Array<string>,
-  content: Array<Array<string>>,
-}
+  header: Array<string>;
+  content: Array<Array<string>>;
+};
 
-const Table1: FunctionComponent<Table1props> = ({header, content}: Table1props) => {
-  const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
-  return (<table key="table">
-      <thead key="head">
-      <tr key="labels"><td key="corner1"></td>
-      {header.map((cell, columnNumber) => (<td key={"label " + columnNumber}>{alphabet[columnNumber]}</td>))}
-      </tr>
-      <tr key="headers"><td key="corner2"></td>
-      {header.map((cell, columnNumber) => (<td key={"header " + columnNumber}>{cell}</td>))}
-      </tr>
-      </thead>
-      <tbody key="body">
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+type TableHeaderProps = {
+  header: Array<string>;
+};
+
+const TableHeader = ({ header }: TableHeaderProps) => (
+  <thead key="head">
+    <tr key="labels">
+      <td key="corner1"></td>
+      {header.map((cell, columnNumber) => (
+        <td key={'label ' + columnNumber}>{alphabet[columnNumber]}</td>
+      ))}
+    </tr>
+    <tr key="headers">
+      <td key="corner2"></td>
+      {header.map((cell, columnNumber) => (
+        <td key={'header ' + columnNumber}>{cell}</td>
+      ))}
+    </tr>
+  </thead>
+);
+
+const Table1: FunctionComponent<Table1props> = ({
+  header,
+  content,
+}: Table1props) => (
+  <table key="table">
+    <TableHeader header={header} />
+    <tbody key="body">
       {content.map((row, rowNumber) => (
-      <tr key={"row" + rowNumber}><td>{rowNumber}</td>
-        {row.map((cell, columnNumber) =>
-              <td key={"cell " + rowNumber + "," + columnNumber}><input type="text" value={cell} key={rowNumber + "," + columnNumber}
-                     name="name"
-                     onChange={event => console.log(rowNumber, columnNumber, event.target.value)}/></td>)}
-     </tr>))} </tbody></table> )
-}
+        <tr key={'row' + rowNumber}>
+          <td>{rowNumber}</td>
+          {row.map((cell, columnNumber) => (
+            <td key={'cell ' + rowNumber + ',' + columnNumber}>
+              <input
+                type="text"
+                value={cell}
+                key={rowNumber + ',' + columnNumber}
+                name="name"
+                onChange={(event) =>
+                  console.log(rowNumber, columnNumber, event.target.value)
+                }
+              />
+            </td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
 
 type InputWithLabelProps = {
-  id: string,
-  label?: string,
-  type?: string,
-  value: string,
-  isFocused: boolean,
-  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+  id: string;
+  label?: string;
+  type?: string;
+  value: string;
+  isFocused: boolean;
+  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   children?: ReactChild | ReactChildren;
-}
+};
 
-const InputWithLabel: FunctionComponent<InputWithLabelProps> =
-  ({id, label, type="text", value, isFocused, onInputChange, children}: InputWithLabelProps) => {
-    const inputRef = React.useRef<HTMLInputElement>(null);
-    React.useEffect(() => {if (isFocused && inputRef.current) {inputRef.current.focus();}}, [isFocused]);
-    return (<>
-        <label htmlFor={id}>{children}</label>&nbsp;
-        <input ref={inputRef} id={id} type={type} value={value} autoFocus={isFocused} onChange={onInputChange} />
-      </>);
-  };
+const InputWithLabel: FunctionComponent<InputWithLabelProps> = ({
+  id,
+  label,
+  type = 'text',
+  value,
+  isFocused,
+  onInputChange,
+  children,
+}: InputWithLabelProps) => {
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>&nbsp;
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        autoFocus={isFocused}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
 
-  const initialStories: article[] = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
+const contentheader = ['Alabel', 'Blabel', 'Clabel'];
+const content = [
+  ['A1', 'B1', 'C1'],
+  ['A2', 'B2', 'C2'],
 ];
 
-type StoryList = {
-  stories: Array<article>,
-};
-
-
-type StoryPromise = {
-  data: StoryList,
-};
-
-const getAsyncStories = () => new Promise<StoryPromise>((resolve) =>
-  setTimeout(() => resolve({data: {stories: initialStories}}), 20000));
-  //setTimeout(reject, 20000));
-
-const App = () => {
-
-  const contentheader = ["Alabel", "Blabel", "Clabel" ]
-  const content = [
-    ["A1","B1","C1"],
-    ["A2","B2","C2"]
-  ]
-
-
+const App = (): JSX.Element => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
   const [stories, dispatchStories] = React.useReducer(storiesReducer, {
     stories: [] as article[],
     isLoading: false,
     isError: false,
   });
-
-  React.useEffect(() => {
-     dispatchStories({type: 'STORIES_FETCH_INIT' });
-     fetch(`${DEMO_API_ENDPOINT}react`)
-      .then((response) =>  response.json())
-      .then((result) => {
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,
-        });
-      })
-      .catch(() => dispatchStories({type: 'STORIES_FETCH_FAILURE' }));
-  }, []);
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
@@ -223,6 +240,19 @@ const App = () => {
       payload: item,
     });
   };
+
+  React.useEffect(() => {
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
+    fetch(`${DEMO_API_ENDPOINT}react`)
+      .then((response) => response.json())
+      .then((result) => {
+        dispatchStories({
+          type: 'STORIES_FETCH_SUCCESS',
+          payload: result.hits,
+        });
+      })
+      .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
+  }, []);
 
   const searchedStories = stories.stories.filter((story: article) => {
     return story.title.toLowerCase().includes(searchTerm.toLowerCase());
