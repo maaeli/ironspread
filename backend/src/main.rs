4158e61bd7257d6e9fe47a4f7b5872912885c406
Martha Brennich <martha.brennich@googlemail.com>
2020-12-18 19:05:46 +0100

    Now can send json

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -10,18 +10,18 @@ struct AppStateWithCounter {
     counter: Mutex<i32>,
 }
 
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Balance {
     date: String,
     balances: Vec<f32>,
 }
 
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct AccountNames {
     names: Vec<String>,
 }
 
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct AccountData {
     account_names: AccountNames,
     balances: Vec<Balance>,
@@ -37,6 +37,18 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
+#[get("/account_data")]
+async fn account_data() -> impl Responder {
+    let account_names = AccountNames{names: vec![String::from("bank a"), String::from("bank b"), String::from("bank c")]};
+    let balance1 = Balance {date: String::from("May 2018"), balances: vec![1.3, 5.6, 7.8]};
+    let balance2 = Balance {date: String::from("June 2018"), balances: vec![-0.3, 2.1, 4.0]};
+
+    let current_balance = AccountData{account_names: account_names, balances: vec![balance1, balance2]};
+    let serialized = serde_json::to_string(&current_balance).unwrap();
+    println!("serialized = {}", serialized);
+    web::Json(current_balance)
+}
+
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
@@ -50,13 +62,6 @@ async fn index(data: web::Data<AppStateWithCounter>) -> String {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let account_names = AccountNames{names: vec![String::from("bank a"), String::from("bank b"), String::from("bank c")]};
-    let balance1 = Balance {date: String::from("May 2018"), balances: vec![1.3, 5.6, 7.8]};
-    let balance2 = Balance {date: String::from("June 2018"), balances: vec![-0.3, 2.1, 4.0]};
-
-    let current_balance = AccountData{account_names: account_names, balances: vec![balance1, balance2]};
-    let serialized = serde_json::to_string(&current_balance).unwrap();
-    println!("serialized = {}", serialized);
 
     let counter = web::Data::new(AppStateWithCounter {
         counter: Mutex::new(0),
@@ -67,6 +72,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(counter.clone())
             .service(hello)
             .service(echo)
+            .service(account_data)
             .route("/hey", web::get().to(manual_hello))
             .route("/index", web::get().to(index))
     })
