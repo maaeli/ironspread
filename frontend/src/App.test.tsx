01477c16509c8a0f271e23f24c9a23884e94e5db
Martha <martha.brennich@googlemail.com>
2021-04-25 10:36:16 +0200

    removed tutorial code

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,55 +2,15 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 import axios from 'axios';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import App, { storiesReducer, StoriesAction } from './App';
-import { article } from './Item';
+import { render, screen, waitFor } from '@testing-library/react';
+import App from './App';
 
 jest.mock('axios');
-
-const reactAuthor = 'Jordan Walke';
-const reduxAuthor = 'Dan Abramov, Andrew Clark';
-const jsAuthor = 'Brendan Eich';
-
-const storyOne: article = {
-  title: 'React',
-  url: 'https://reactjs.org/',
-  author: reactAuthor,
-  num_comments: 3,
-  points: 4,
-  objectID: 0,
-};
-
-const storyTwo: article = {
-  title: 'Redux',
-  url: 'https://redux.js.org/',
-  author: reduxAuthor,
-  num_comments: 2,
-  points: 5,
-  objectID: 1,
-};
-
-const anotherStory = {
-  title: 'Javascript',
-  url: 'http://en.wikipedia.org/wiki/Javascript',
-  author: jsAuthor,
-  num_comments: 15,
-  points: 10,
-  objectId: 3,
-};
-
-const stories = [storyOne, storyTwo];
 
 const mockSwitch = (
   url: string,
 ): Promise<Record<string, unknown>> | undefined => {
-  if (url.includes('react')) {
-    return Promise.resolve({
-      data: {
-        hits: stories,
-      },
-    });
-  } else if (url.includes('account_data')) {
+  if (url.includes('account_data')) {
     return Promise.resolve({
       data: {
         account_names: ['al', 'bl'],
@@ -60,117 +20,17 @@ const mockSwitch = (
         ],
       },
     });
-  } else if (url.includes('JavaScript')) {
-    return Promise.resolve({
-      data: {
-        hits: [anotherStory],
-      },
-    });
   }
   return undefined;
 };
-
-describe('storiesReducer', () => {
-  test('removes a story from all stories', () => {
-    const action: StoriesAction = { type: 'REMOVE_STORIES', payload: storyOne };
-    const state = { stories: stories, isLoading: false, isError: false };
-    const newState = storiesReducer(state, action);
-    const expectedState = {
-      stories: [storyTwo],
-      isLoading: false,
-      isError: false,
-    };
-    expect(newState).toStrictEqual(expectedState);
-  });
-});
 
 describe('App', () => {
   test('succeeds fetching data', async () => {
     const axiosMock = (axios.get as jest.Mock).mockImplementation(mockSwitch);
 
     render(<App />);
-    expect(screen.queryByText(/Loading/)).toBeInTheDocument();
     expect(screen.queryByText(/Waiting for data/)).toBeInTheDocument();
-    await waitFor(() => expect(axiosMock).toHaveBeenCalledTimes(2));
-    expect(screen.queryByText(/Loading/)).toBeNull;
+    await waitFor(() => expect(axiosMock).toHaveBeenCalledTimes(1));
     expect(screen.queryByText(/Waiting for data/)).not.toBeInTheDocument();
-    expect(screen.getByText('React')).toBeInTheDocument();
-    expect(screen.getByText('Redux')).toBeInTheDocument();
-    expect(screen.getAllByRole('button').length).toBe(3);
-  });
-
-  test('fails fetching stories data', async () => {
-    const axiosMock = (axios.get as jest.Mock).mockImplementation(
-      (url: string) => {
-        if (url.includes('react')) {
-          return Promise.reject(new Error('Async error'));
-        }
-        if (url.includes('account_data')) {
-          return Promise.resolve({
-            data: {
-              account_names: ['al', 'bl'],
-              balances: [
-                { date: 'one', balances: [4, 1.1] },
-                { date: 'two', balances: [5, 8.9] },
-              ],
-            },
-          });
-        }
-      },
-    );
-
-    render(<App />);
-    expect(screen.queryByText(/Loading/)).toBeInTheDocument();
-
-    await waitFor(() => expect(axiosMock).toHaveBeenCalledTimes(2));
-    expect(screen.queryByText(/Loading/)).toBeNull;
-    expect(screen.queryByText(/went wrong/)).toBeInTheDocument();
-  });
-
-  test('removes a story', async () => {
-    const axiosMock = (axios.get as jest.Mock).mockImplementation(mockSwitch);
-
-    render(<App />);
-
-    await waitFor(() => expect(axiosMock).toHaveBeenCalledTimes(2));
-    expect(screen.getByText(reactAuthor)).toBeInTheDocument();
-    expect(screen.getAllByRole('button').length).toBe(3);
-
-    fireEvent.click(screen.getAllByRole('button')[1]);
-    expect(screen.queryByText(reactAuthor)).toBeNull();
-    expect(screen.getAllByRole('button').length).toBe(2);
-  });
-
-  test('searches for specific stories', async () => {
-    const axiosMock = (axios.get as jest.Mock).mockImplementation(mockSwitch);
-
-    render(<App />);
-
-    await waitFor(() => expect(axiosMock).toHaveBeenCalledTimes(2));
-    expect(screen.queryByText('react')).toBeInTheDocument();
-    expect(screen.queryByText('JavaScript')).toBeNull();
-    expect(screen.queryByText(reactAuthor)).toBeInTheDocument();
-    expect(screen.queryByText(reduxAuthor)).toBeInTheDocument();
-    expect(screen.queryByText(jsAuthor)).toBeNull();
-
-    fireEvent.change(screen.getByDisplayValue('react'), {
-      target: {
-        value: 'JavaScript',
-      },
-    });
-    expect(screen.queryByText('react')).toBeNull();
-    await waitFor(() =>
-      expect(screen.queryByText('JavaScript')).toBeInTheDocument(),
-    );
-    fireEvent.submit(screen.getByText('Submit'));
-    expect(screen.queryByText('JavaScript')).toBeInTheDocument();
-    await waitFor(() => expect(axiosMock).toHaveBeenCalledTimes(3));
-    expect(screen.queryByText(reactAuthor)).toBeNull();
-    expect(screen.queryByText(reduxAuthor)).toBeNull();
-    expect(screen.queryByText(jsAuthor)).toBeInTheDocument();
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
   });
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,75 +1,8 @@
-import React, {
-  FunctionComponent,
-  ReactElement,
-  Dispatch,
-  SetStateAction,
-} from 'react';
+import React from 'react';
 import axios from 'axios';
-import Item, { article } from './Item';
-import SearchForm from './SearchForm';
 import Table, { TableProps } from './Table';
 import './App.css';
 import { parse_account_json_to_table, AccountBalancesFromServer } from './data';
-
-const DEMO_API_ENDPOINT = 'http://hn.algolia.com/api/v1/search?query=';
-
-const useSemiPersistentState = (
-  key: string,
-  initialState: string,
-): [string, Dispatch<SetStateAction<string>>] => {
-  const [value, setValue] = React.useState(
-    localStorage.getItem(key) || initialState,
-  );
-
-  React.useEffect(() => {
-    localStorage.setItem(key, value as string);
-  }, [value, key]);
-  return [value as string, setValue as Dispatch<SetStateAction<string>>];
-};
-
-type StoriesAction =
-  | { type: 'STORIES_FETCH_INIT' }
-  | { type: 'STORIES_FETCH_SUCCESS'; payload: Array<article> }
-  | { type: 'STORIES_FETCH_FAILURE' }
-  | { type: 'REMOVE_STORIES'; payload: article };
-
-type StoriesState = {
-  stories: Array<article>;
-  isLoading: boolean;
-  isError: boolean;
-};
-
-const storiesReducer = (
-  state: StoriesState,
-  action: StoriesAction,
-): StoriesState => {
-  switch (action.type) {
-    case 'STORIES_FETCH_INIT':
-      return { ...state, isLoading: true, isError: false };
-    case 'STORIES_FETCH_SUCCESS':
-      return {
-        ...state,
-        stories: action.payload,
-        isLoading: false,
-        isError: false,
-      };
-    case 'STORIES_FETCH_FAILURE':
-      return {
-        ...state,
-        isLoading: false,
-        isError: true,
-      };
-    case 'REMOVE_STORIES':
-      return {
-        ...state,
-        stories: state.stories.filter(
-          (story) => action.payload.objectID !== story.objectID,
-        ),
-      };
-    default:
-      throw new Error();
-  }
-};
 
 type AccountDataAction =
   | { type: 'ACCOUNT_DATA_FETCH_INIT' }
@@ -98,22 +31,6 @@ const accountDataReducer = (
   }
 };
 
-type ListProps = {
-  list: Array<article>;
-  onRemoveItem: (item: article) => void;
-};
-
-const List: FunctionComponent<ListProps> = ({
-  list,
-  onRemoveItem,
-}: ListProps): ReactElement | null => (
-  <>
-    {list.map((item: article) => (
-      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
-    ))}
-  </>
-);
-
 const contentheader = ['Alabel', 'Blabel', 'Clabel'];
 const content = [
   ['A1', 'B1', 'C1'],
@@ -123,13 +40,6 @@ const content = [
 /* eslint-disable max-lines-per-function */
 
 const App = (): JSX.Element => {
-  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'react');
-  const [stories, dispatchStories] = React.useReducer(storiesReducer, {
-    stories: [] as article[],
-    isLoading: false,
-    isError: false,
-  });
-
   const [account_data, dispatch_account_data] = React.useReducer(
     accountDataReducer,
     {
@@ -139,41 +49,7 @@ const App = (): JSX.Element => {
     },
   );
 
-  const [url, setUrl] = React.useState(`${DEMO_API_ENDPOINT}${searchTerm}`);
-
   const demo = '';
-
-  const handleSearchInput = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ): void => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSearchSubmit = (event: React.FormEvent): void => {
-    setUrl(`${DEMO_API_ENDPOINT}${searchTerm}`);
-    event.preventDefault();
-  };
-
-  const handleRemoveStory = (item: article): void => {
-    dispatchStories({
-      type: 'REMOVE_STORIES',
-      payload: item,
-    });
-  };
-
-  const handleFetchStories = React.useCallback(() => {
-    if (!searchTerm) return;
-    dispatchStories({ type: 'STORIES_FETCH_INIT' });
-    axios
-      .get(url)
-      .then((result) => {
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.hits,
-        });
-      })
-      .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
-  }, [url as string]);
 
   const handleFetchAccountData = React.useCallback(() => {
     dispatch_account_data({ type: 'ACCOUNT_DATA_FETCH_INIT' });
@@ -191,29 +67,11 @@ const App = (): JSX.Element => {
   }, [demo as string]);
 
   React.useEffect(() => {
-    handleFetchStories();
-  }, [url as string]);
-
-  React.useEffect(() => {
     handleFetchAccountData();
   }, [demo as string]);
 
   return (
     <div className="container">
-      <h1 className="headline-primary">My story</h1>
-      <SearchForm
-        searchTerm={searchTerm}
-        onSearchInput={handleSearchInput}
-        onSearchSubmit={handleSearchSubmit}
-      />
-      {stories.isError && <p>Something went wrong ... </p>}
-      {stories.isLoading ? (
-        <p>Loading ...</p>
-      ) : (
-        <>
-          <List list={stories.stories} onRemoveItem={handleRemoveStory} />
-        </>
-      )}
       {account_data.isLoading ? (
         <p>Waiting for data ...</p>
       ) : (
@@ -231,4 +89,3 @@ const App = (): JSX.Element => {
 /* eslint-enable max-lines-per-function */
 
 export default App;
-export { storiesReducer, StoriesState, StoriesAction, List };
